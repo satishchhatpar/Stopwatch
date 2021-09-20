afcdab32817db87d3f5a11547bf45e75698f5eb9
satishchhatpar <satish.chhatpar@gmail.com>
2021-09-20 11:36:53 +0530

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Satish Chhatpar on 20/09/21.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

73351b0da2df15000b6ab225a3ae1dea0bf04a69
satishchhatpar <satish.chhatpar@gmail.com>
2021-09-28 01:59:43 +0530

    Commit

---
+++ Stopwatch/Unused Views/StopwatchColor.swift
@@ -0,0 +1,13 @@
+//
+//  StopwatchColor.swift
+//  Stopwatch
+//
+//  Created by Satish Chhatpar on 23/09/21.
+//
+
+import SwiftUI
+
+struct StopwatchColor {
+    static let main = Color(red: 20/255, green: 28/255, blue: 58/255)
+    static let accent = Color(red: 48/255, green: 105/255, blue: 240/255)
+}
